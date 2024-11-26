2147cb5f0e6cb733363afe01dd6c25d06f6e8613
yeeunli <lye920@g.hongik.ac.kr>
2024-11-26 19:41:13 +0900

    chore: spring boot 프로젝트 생성

---
+++ src/main/java/com/example/study_group/StudyGroupApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyGroupApplication.class, args);
+	}
+
+}
