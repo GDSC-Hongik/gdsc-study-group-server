0b6b612d1220cfeab9e4fbc2c62d77f67fa7c327
CheonMinJae <cmjcmj1011@gmail.com>
2024-12-12 15:45:39 +0900

    chore: pr random reviewer 기능

---
+++ .github/workflows/random_reviewer/reviewer.js
@@ -0,0 +1,60 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const member= {
+    cmj7271: "open_mind_",
+    ybkang1108: "yub_in_",
+    yeeunli: "yeeun4665",
+    JungJaehoon0430: "jaehoon0430"
+}
+
+function selectRandomReviewer() {
+    const candidate = Object.keys(member).filter(
+        (name) => name !== github.context.actor
+    )
+    return candidate[
+        Math.floor(Math.random() * candidate.length)
+        ]
+}
+
+async function sendDiscordMsg(reviewer) {
+    const webhook = process.env.DISCORD_WEBHOOK;
+
+    const msg = {
+        content: createMsg(reviewer)
+    }
+
+    await fetch(webhook, {
+        method: "POST",
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(msg)
+    })
+}
+
+function createMsg(reviewer) {
+    return "리뷰해주세요\n" + "* PR " + `https://github.com/${github.context.repo.owner}/
+    ${github.context.repo.repo}/pulls/${github.context.payload.pull_request.number}`
+        + "\n* 담당자: " + member[reviewer]
+}
+
+async function main() {
+    const githubClient = github.getOctokit(process.env.REVIEW_TOKEN);
+    const reviewer = selectRandomReviewer();
+
+    githubClient.rest.pulls.requestReviewers(
+        {
+            owner: github.context.repo.owner,
+            repo: github.context.repo,
+            pull_number: github.context.payload.pull_request.number,
+            reviewers: [reviewer]
+        }
+    )
+        .then((res) => console.log("reviewer assign success: ", res))
+        .catch((err) => console.log("reviewer assign failed:", err));
+
+    sendDiscordMsg(reviewer)
+        .then(() => console.log("message send success"))
+        .catch(() => console.log("message send failed"));
+}
+
+main().then(() => console.log("success")).catch(() => console.log("failed"));
