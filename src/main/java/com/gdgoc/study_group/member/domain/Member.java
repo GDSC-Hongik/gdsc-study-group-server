3d2d4398a1a25b5c8f13a4f6f853fe323b26f8d6
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-08 01:32:55 +0900

    member domain 추가

---
+++ src/main/java/com/gdgoc/study_group/member/domain/Member.java
@@ -0,0 +1,25 @@
+package com.gdgoc.study_group.member.domain;
+
+import com.gdgoc.study_group.studyMember.domain.StudyMember;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class Member {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToMany
+  @JoinColumn(name = "study_member_id")
+  private List<StudyMember> studyMembers;
+
+  private String name;
+  private String github;
+  private String studentNumber;
+}
