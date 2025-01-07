6cdb08d096bf813ddc5615cde0b85abc2874d32a
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-07 18:03:01 +0900

    docker compose 에 db 연결

--- src/main/java/com/gdgoc/study_group/StudyGroupApplication.java
+++ src/main/java/com/gdgoc/study_group/StudyGroupApplication.java
@@ -1,4 +1,4 @@
-package com.example.study_group;
+package com.gdgoc.study_group;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudyGroupApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudyGroupApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(StudyGroupApplication.class, args);
+  }
 }
