60ebd32cfda1aceca3d8843e2aec88cdff8fb4a9
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-08 15:40:42 +0900

    test

--- src/main/java/com/gdgoc/study_group/StudyGroupApplication.java
+++ src/main/java/com/gdgoc/study_group/StudyGroupApplication.java
@@ -8,6 +8,7 @@ public class StudyGroupApplication {
 
   public static void main(String[] args) {
 
-    SpringApplication.run(StudyGroupApplication.class, args);
-  }
+
+
+    SpringApplication.run(StudyGroupApplication.class, args); }
 }
