54914389f19369eacac8a039e0a8798e16bb33da
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-08 21:36:46 +0900

    study 도메인 추가

--- src/main/java/com/gdgoc/study_group/study/domain/Study.java
+++ src/main/java/com/gdgoc/study_group/study/domain/Study.java
@@ -1,11 +1,14 @@
 package com.gdgoc.study_group.study.domain;
 
+import com.gdgoc.study_group.answer.domain.Answer;
+import com.gdgoc.study_group.curriculum.domain.Curriculum;
+import com.gdgoc.study_group.day.domain.Day;
+import com.gdgoc.study_group.round.domain.Round;
 import com.gdgoc.study_group.studyMember.domain.StudyMember;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import java.util.List;
 
@@ -15,9 +18,25 @@ public class Study {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @OneToMany
-  @JoinColumn(name = "study_member_id")
+  @OneToMany(mappedBy = "study_member_id")
   private List<StudyMember> studyMembers;
 
+  @OneToMany(mappedBy = "round_id")
+  private List<Round> rounds;
+
+  @OneToMany(mappedBy = "curriculum_id")
+  private List<Curriculum> curriculums;
+
+  @OneToMany(mappedBy = "day_id")
+  private List<Day> days;
+
+  @OneToMany(mappedBy = "answer_id")
+  private List<Answer> answers;
+
   private String name;
+  private String description;
+  private boolean isOffline;
+  private boolean isActive;
+  private Integer participants; // 현재 스터디 참여 인원
+  private Integer maxParticipants; // null == 인원 제한 X
 }
