cc19b1fc19dab3881d92128d030a6dc5ebfb1d60
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-08 23:13:06 +0900

    roundMember domain 추가

---
+++ src/main/java/com/gdgoc/study_group/roundMember/domain/RoundMember.java
@@ -0,0 +1,27 @@
+package com.gdgoc.study_group.roundMember.domain;
+
+import com.gdgoc.study_group.member.domain.Member;
+import com.gdgoc.study_group.round.domain.Round;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class RoundMember {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "round_id")
+  private Round round;
+
+  @ManyToOne
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  private String retrospect; // 후기 내용
+}
