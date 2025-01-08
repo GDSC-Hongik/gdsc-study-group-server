9ed8062a73dcedfcf5ccc9cc79fa9833249b15d6
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-08 20:23:07 +0900

    StudyMember 에 대해 Member, Study 에 대한 JoinColumn 추가

--- src/main/java/com/gdgoc/study_group/studyMember/domain/StudyMember.java
+++ src/main/java/com/gdgoc/study_group/studyMember/domain/StudyMember.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
@@ -16,9 +17,13 @@ public class StudyMember {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @ManyToOne private Member member;
+  @ManyToOne
+  @JoinColumn(name = "member_id")
+  private Member member;
 
-  @ManyToOne private Study study;
+  @ManyToOne
+  @JoinColumn(name = "study_id")
+  private Study study;
 
   @Enumerated(EnumType.STRING)
   private Status status;
