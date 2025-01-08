5159dac86a7e52a2b0673d64c4b624eef5540b0f
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-08 15:38:08 +0900

    test

--- src/main/java/com/gdgoc/study_group/StudyGroupApplication.java
+++ src/main/java/com/gdgoc/study_group/StudyGroupApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudyGroupApplication {
 
   public static void main(String[] args) {
+
     SpringApplication.run(StudyGroupApplication.class, args);
   }
 }
