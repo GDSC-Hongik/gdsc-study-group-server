05c9edd4192017a051bece561c0142ecef193a85
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-10 18:56:27 +0900

    study 도메인에 지원 질문 필드 추가
    지원 질문은 nullable
    null 이라면, 지원 답변 없이 신청 가능

--- src/main/java/com/gdgoc/study_group/study/domain/Study.java
+++ src/main/java/com/gdgoc/study_group/study/domain/Study.java
@@ -36,6 +36,7 @@ public class Study {
 
   private String name;
   private String description;
+  private String question; // 지원 질문, nullable: 지원 답변 없이 바로 신청 가능
   private boolean isOffline;
   private boolean isActive;
   private Integer participants; // 현재 스터디 참여 인원
