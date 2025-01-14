be2464b5d7ed9a750cf72c0be3f1f2da6e23f431
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-14 16:57:11 +0900

    특정 멤버, 특정 상태를 가진 스터디 목록 쿼리 수정
    StudyMember 가 아닌 Study 를 반환하도록 수정

--- src/main/java/com/gdgoc/study_group/member/dao/MemberRepository.java
+++ src/main/java/com/gdgoc/study_group/member/dao/MemberRepository.java
@@ -1,8 +1,8 @@
 package com.gdgoc.study_group.member.dao;
 
 import com.gdgoc.study_group.member.domain.Member;
-import com.gdgoc.study_group.study.domain.StudyStatus;
-import com.gdgoc.study_group.studyMember.domain.StudyMember;
+import com.gdgoc.study_group.study.domain.Study;
+import com.gdgoc.study_group.studyMember.domain.StudyMemberStatus;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,10 +11,19 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+  /**
+   * 해당 멤버가 해당 상태를 가진 스터디 목록들을 반환합니다.
+   *
+   * @param memberId 검색할 대상이 되는 멤버의 PK
+   * @param memberStatus 검색할 대상이 되는 멤버가 스터디에서 가지길 기대하는 상태
+   * @return 해당 멤버가 포함되고, 해당 상태를 상태로 가지는 스터디들을 반환합니다.
+   */
   @Query(
-      "SELECT s FROM StudyMember s WHERE s.member.id = :id AND s.studyMemberStatus = :studyStatus")
-  List<StudyMember> findByMemberIdAndStatus(
-      @Param("id") Long id, @Param("studyStatus") StudyStatus studyStatus);
+      "SELECT sm.study FROM StudyMember sm WHERE"
+          + " sm.member.id = :id AND"
+          + " sm.studyMemberStatus = :memberStatus")
+  List<Study> findByMemberIdAndStatus(
+      @Param("id") Long memberId, @Param("memberStatus") StudyMemberStatus memberStatus);
 
   Member findByGithub(String github);
 }
