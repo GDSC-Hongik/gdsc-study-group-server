12518acf507f8f83abdd21b36545b9e38783ded8
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-14 16:23:45 +0900

    MemberRepository 에 custom query 추가
    member id 와 멤버 상태가 일치하는 studyMember 를 찾는 query

--- src/main/java/com/gdgoc/study_group/member/dao/MemberRepository.java
+++ src/main/java/com/gdgoc/study_group/member/dao/MemberRepository.java
@@ -1,11 +1,20 @@
 package com.gdgoc.study_group.member.dao;
 
 import com.gdgoc.study_group.member.domain.Member;
+import com.gdgoc.study_group.study.domain.StudyStatus;
+import com.gdgoc.study_group.studyMember.domain.StudyMember;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+  @Query(
+      "SELECT s FROM StudyMember s WHERE s.member.id = :id AND s.studyMemberStatus = :studyStatus")
+  List<StudyMember> findByMemberIdAndStatus(
+      @Param("id") Long id, @Param("studyStatus") StudyStatus studyStatus);
 
   Member findByGithub(String github);
 }
