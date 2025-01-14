6b29bf1e0d45758e0c70e9dfbbfa5455170880d4
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-01-14 17:07:07 +0900

    feat: Add Day Repository
    # Conflicts:
    #	src/main/java/com/gdgoc/study_group/day/dao/DayRepository.java

--- src/main/java/com/gdgoc/study_group/day/dao/DayRepository.java
+++ src/main/java/com/gdgoc/study_group/day/dao/DayRepository.java
@@ -1,8 +1,34 @@
 package com.gdgoc.study_group.day.dao;
 
 import com.gdgoc.study_group.day.domain.Day;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DayRepository extends CrudRepository<Day, Long> {}
+public interface DayRepository extends CrudRepository<Day, Long> {
+
+    /**
+     * 스터디 요일을 가져옴
+     *
+     * @param studyId 스터디 아이디
+     * @return 스터디 요일
+     */
+    @Query(value = "select d.day from Day d " +
+            "where d.study.id = :studyId")
+    String getStudyDay(@Param("studyId") Long studyId);
+
+
+    /**
+     * 스터디 시간을 가져옴
+     *
+     * @param studyId 스터디 아이디
+     * @return 스터디 시간
+     */
+    @Query(value = "select d.startTime from Day d " +
+            "where d.study.id = :studyId")
+    String getStudyStartTime(@Param("studyId") Long studyId);
+
+
+}
