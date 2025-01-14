ecf32e5baf3a2bb56d17ae9c396aa609ba349beb
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-01-14 16:53:32 +0900

    feat: Add Answer Repository

--- src/main/java/com/gdgoc/study_group/answer/dao/AnswerRepository.java
+++ src/main/java/com/gdgoc/study_group/answer/dao/AnswerRepository.java
@@ -1,18 +1,15 @@
-package com.gdgoc.study_group.answer.repository;
+package com.gdgoc.study_group.answer.dao;
 
 import com.gdgoc.study_group.answer.domain.Answer;
-import com.gdgoc.study_group.member.domain.Member;
-import com.gdgoc.study_group.study.domain.Study;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
-    boolean existsByMemberIdAndStudyId(Long memberId, Long studyId);
+  boolean existsByMemberIdAndStudyId(Long memberId, Long studyId);
 
-    // 스터디 신청 답변 조회
-    List<Answer> findByStudyId(Long studyId);
+  // 스터디 신청 답변 조회
+  List<Answer> findByStudyId(Long studyId);
 }
