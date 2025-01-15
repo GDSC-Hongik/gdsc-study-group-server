7ce96ff7afb388bbede63c88ef9ba71da0eda0d1
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-15 15:32:11 +0900

    comment repository 제거
    comment repository 의 쿼리를
    round repository 로 이동

--- src/main/java/com/gdgoc/study_group/round/dao/RoundRepository.java
+++ src/main/java/com/gdgoc/study_group/round/dao/RoundRepository.java
@@ -1,8 +1,21 @@
 package com.gdgoc.study_group.round.dao;
 
+import com.gdgoc.study_group.comment.domain.Comment;
 import com.gdgoc.study_group.round.domain.Round;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoundRepository extends JpaRepository<Round, Long> {}
+public interface RoundRepository extends JpaRepository<Round, Long> {
+  /**
+   * 회차의 모든 댓글 조회
+   *
+   * @param roundId 라운드 아이디
+   * @return 해당 라운드의 모든 댓글 리스트
+   */
+  @Query(value = "select c from Comment c where c.round.id = :roundId")
+  List<Comment> findComments(@Param("roundId") Long roundId);
+}
