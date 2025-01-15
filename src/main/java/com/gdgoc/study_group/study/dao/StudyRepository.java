e847fa4e6198bcbb85db893b8f744d90edf53da4
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-15 15:57:20 +0900

    round repository 의 쿼리를 study 로 이동
    findCurriculums, findCurriculumByWeek 메소드 이동

--- src/main/java/com/gdgoc/study_group/study/dao/StudyRepository.java
+++ src/main/java/com/gdgoc/study_group/study/dao/StudyRepository.java
@@ -1,6 +1,7 @@
 package com.gdgoc.study_group.study.dao;
 
 import com.gdgoc.study_group.answer.domain.Answer;
+import com.gdgoc.study_group.curriculum.domain.Curriculum;
 import com.gdgoc.study_group.study.domain.Study;
 import com.gdgoc.study_group.study.domain.StudyStatus;
 import java.util.List;
@@ -12,6 +13,25 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudyRepository extends JpaRepository<Study, Long> {
   /**
+   * 스터디의 모든 커리큘럼을 조회합니다
+   *
+   * @param studyId 스터디 아이디
+   * @return 커리큘럼 List
+   */
+  @Query(value = "select c from Curriculum c where c.study.id = :studyId")
+  List<Curriculum> findCurriculums(@Param("studyId") Long studyId);
+
+  /**
+   * 스터디의 특정 회차의 Curriculum 을 조회합니다
+   *
+   * @param studyId 스터디 아이디
+   * @param week 조회할 회차
+   * @return 해당 회차의 Curriculum
+   */
+  @Query("select c from Curriculum c where" + " c.study.id = :studyId AND" + " c.week = :week")
+  Curriculum findCurriculumByWeek(@Param("studyId") Long studyId, @Param("week") Integer week);
+
+  /**
    * 해당 스터디의 모든 답변을 조회합니다
    *
    * @param studyId 검색할 스터디의 id
@@ -21,7 +41,7 @@ public interface StudyRepository extends JpaRepository<Study, Long> {
   List<Answer> findAnswers(@Param("studyId") Long studyId);
 
   /**
-   * 해당하는 상태를 가진 모든 스터디를 조회합니다
+   * 해당하는 스터디 상태를 가진 모든 스터디를 조회합니다
    *
    * @param studyStatus 조회를 원하는 스터디의 상태
    * @return 해당 상태를 가진 스터디 List
