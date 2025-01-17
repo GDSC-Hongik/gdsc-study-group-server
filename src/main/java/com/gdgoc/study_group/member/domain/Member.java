08c6823a09f3015807096ae9a3552edc08c0b1e8
yeeunli <lye920@g.hongik.ac.kr>
2025-01-17 17:19:10 +0900

    chore: 멤버 도메인에 멤버 수정 로직 추가

--- src/main/java/com/gdgoc/study_group/member/domain/Member.java
+++ src/main/java/com/gdgoc/study_group/member/domain/Member.java
@@ -9,8 +9,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.*;
 
 @Entity
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class Member {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +31,16 @@ public class Member {
   private String name;
   private String github;
   private String studentNumber;
+
+  public void updateMember(String name, String github, String studentNumber) {
+    if (name != null) {
+      this.name = name;
+    }
+    if (github != null) {
+      this.github = github;
+    }
+    if (studentNumber != null) {
+      this.studentNumber = studentNumber;
+    }
+  }
 }
