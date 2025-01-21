e45b8ad795055b8ce08001e6664c9397bd54c271
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-01-21 16:53:32 +0900

    feat: exception 추가

--- src/main/java/com/gdgoc/study_group/exception/ErrorCode.java
+++ src/main/java/com/gdgoc/study_group/exception/ErrorCode.java
@@ -1,4 +1,14 @@
 package com.gdgoc.study_group.exception;
 
-public class ErrorCode {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 에러가 발생했습니다."),
+    ;
+    private final HttpStatus status;
+    private final String message;
 }
--- src/main/java/com/gdgoc/study_group/exception/ErrorResponse.java
+++ src/main/java/com/gdgoc/study_group/exception/ErrorResponse.java
@@ -1,4 +1,16 @@
 package com.gdgoc.study_group.exception;
 
-public class ErrorResponse {
+public record ErrorResponse (String errorCodeName, String errorMessage){
+
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(errorCode.name(), errorCode.getMessage());
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode, String errorMessage) {
+        return new ErrorResponse(errorCode.name(), errorMessage);
+    }
+
+    public static ErrorResponse of(String errorCodeName, String errorMessage) {
+        return new ErrorResponse(errorCodeName, errorMessage);
+    }
 }
