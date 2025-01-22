86e6b33a42172c5df309e14a58ed72743d65d65d
yeeunli <lye920@g.hongik.ac.kr>
2025-01-22 19:51:44 +0900

    refactor: Member 도메인 생성자 접근 제어자 protected로 추가

--- src/main/java/com/gdgoc/study_group/member/domain/Member.java
+++ src/main/java/com/gdgoc/study_group/member/domain/Member.java
@@ -14,7 +14,7 @@ import lombok.*;
 @Entity
 @Builder
 @ToString
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class Member {
