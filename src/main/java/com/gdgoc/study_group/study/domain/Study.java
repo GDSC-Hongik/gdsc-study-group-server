047112bb01655b5dfdb2bb26a56b95944456c6bc
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-01-23 00:12:31 +0900

    feat: study 도메인에 create, update 추가

--- src/main/java/com/gdgoc/study_group/study/domain/Study.java
+++ src/main/java/com/gdgoc/study_group/study/domain/Study.java
@@ -11,10 +11,8 @@ import java.util.List;
 import lombok.*;
 
 @Entity
-@Builder
 @Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Study {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -44,4 +42,57 @@ public class Study {
   private String question; // 지원 질문, nullable: 지원 답변 없이 바로 신청 가능
   private Integer maxParticipants; // null == 인원 제한 X
   private Boolean isApplicationClosed = false; // 멤버 지원 종료 여부(기본값은 지원 가능)
+
+  @Builder(access = AccessLevel.PRIVATE)
+  private Study(
+      String name,
+      String description,
+      String requirement,
+      String question,
+      Integer maxParticipants,
+      StudyStatus studyStatus) {
+    this.name = name;
+    this.description = description;
+    this.requirement = requirement;
+    this.question = question;
+    this.maxParticipants = maxParticipants;
+    this.studyStatus = studyStatus;
+  }
+
+  public static Study create(
+      String name,
+      String description,
+      String requirement,
+      String question,
+      Integer maxParticipants,
+      StudyStatus status) {
+    return Study.builder()
+        .name(name)
+        .description(description)
+        .requirement(requirement)
+        .question(question)
+        .maxParticipants(maxParticipants)
+        .studyStatus(status)
+        .build();
+  }
+
+  public void addInfo(List<Curriculum> curriculums, List<Day> days) {
+    this.curriculums.addAll(curriculums);
+    this.days.addAll(days);
+  }
+
+  public void update(
+      String name,
+      String description,
+      String requirement,
+      String question,
+      Integer maxParticipants,
+      StudyStatus studyStatus) {
+    this.name = name;
+    this.description = description;
+    this.requirement = requirement;
+    this.question = question;
+    this.maxParticipants = maxParticipants;
+    this.studyStatus = studyStatus;
+  }
 }
