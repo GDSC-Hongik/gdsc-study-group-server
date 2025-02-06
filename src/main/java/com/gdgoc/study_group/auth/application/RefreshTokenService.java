13f7835ea52e382d604cf672486641fbd922d75d
yeeunli <lye920@g.hongik.ac.kr>
2025-02-06 17:10:58 +0900

    fix: Auth 재로그인 시 기존 refresh 토큰 db에서 삭제

--- src/main/java/com/gdgoc/study_group/auth/application/RefreshTokenService.java
+++ src/main/java/com/gdgoc/study_group/auth/application/RefreshTokenService.java
@@ -18,11 +18,22 @@ public class RefreshTokenService {
     private final RefreshRepository refreshRepository;
     private final AuthRepository authRepository;
 
+    /**
+     * 로그인 시 생성되는 리프레쉬 토큰을 데이터베이스에 저장합니다.
+     *
+     * @param authId 회원 정보
+     * @param refreshToken 저장하려는 리프레쉬 토큰
+     * @param expiredMs 저장하려는 리프레쉬 토큰의 만료 시간
+     */
     @Transactional
     public void saveRefresh(Long authId, String refreshToken, Long expiredMs) {
 
         Auth auth = authRepository.findById(authId)
                 .orElseThrow(() -> new NoSuchElementException("인증 정보를 찾을 수 없습니다."));
+
+        // 기존 사용자의 refresh 토큰이 있다면 삭제
+        refreshRepository.deleteByAuth(auth);
+        refreshRepository.flush();
 
         Date date = new Date(System.currentTimeMillis() + expiredMs);
 
--- src/main/java/com/gdgoc/study_group/auth/dao/RefreshRepository.java
+++ src/main/java/com/gdgoc/study_group/auth/dao/RefreshRepository.java
@@ -1,7 +1,18 @@
 package com.gdgoc.study_group.auth.dao;
 
+import com.gdgoc.study_group.auth.domain.Auth;
 import com.gdgoc.study_group.auth.domain.Refresh;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 public interface RefreshRepository extends JpaRepository<Refresh, Long> {
+
+    @Transactional(readOnly = false)
+    void deleteByAuth(Auth auth);
+
+    Boolean existsByRefresh(String refresh);
+
+    @Transactional(readOnly = false)
+    void deleteByRefresh(String refresh);
 }
