79fd200843dc7238fa2071e163742f20d6bd501a
yeeunli <lye920@g.hongik.ac.kr>
2025-02-06 17:32:43 +0900

    feat: Auth 로그아웃 구현

--- src/main/java/com/gdgoc/study_group/auth/config/SecurityConfig.java
+++ src/main/java/com/gdgoc/study_group/auth/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.gdgoc.study_group.auth.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gdgoc.study_group.auth.application.RefreshTokenService;
+import com.gdgoc.study_group.auth.dao.RefreshRepository;
+import com.gdgoc.study_group.auth.jwt.CustomLogoutFilter;
 import com.gdgoc.study_group.auth.jwt.JwtFilter;
 import com.gdgoc.study_group.auth.jwt.JwtUtil;
 import com.gdgoc.study_group.auth.jwt.LoginFilter;
@@ -17,6 +19,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -37,6 +40,7 @@ public class SecurityConfig {
     private final AuthenticationConfiguration authenticationConfiguration;
     private final ObjectMapper objectMapper;
     private final JwtUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
@@ -81,6 +85,10 @@ public class SecurityConfig {
                 new LoginFilter(objectMapper ,authenticationManager(authenticationConfiguration), jwtUtil, refreshTokenService),
                 UsernamePasswordAuthenticationFilter.class
         );
+
+        // CustomLogoutFilter 추가
+        http
+                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class);
 
         // 세션을 stateless한 상태로 진행하기 위함
         http
---
+++ src/main/java/com/gdgoc/study_group/auth/jwt/CustomLogoutFilter.java
@@ -0,0 +1,103 @@
+package com.gdgoc.study_group.auth.jwt;
+
+import com.gdgoc.study_group.auth.dao.RefreshRepository;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class CustomLogoutFilter extends GenericFilterBean {
+
+    private final JwtUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+
+        doFilter((HttpServletRequest) request, (HttpServletResponse) response, filterChain);
+    }
+
+    /**
+     * 로그아웃 관련이 아닌 부적절한 요청이라면 다음 필터로 넘깁니다.
+     */
+    private void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+
+        // 경로 검증
+        String requestURI = request.getRequestURI();
+        if (!requestURI.matches("^\\/auth\\/logout$")) {
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // method 검증
+        String requestMethod = request.getMethod();
+        if (!requestMethod.equals("POST")) {
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 리프레쉬 토큰 검증
+        String refresh = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+
+            if (cookie.getName().equals("refresh")) {
+                refresh = cookie.getValue();
+            }
+        }
+
+        if (refresh == null) {
+
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        try {
+            jwtUtil.isExpired(refresh);
+        } catch (ExpiredJwtException e) {
+
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        String category = jwtUtil.getCategory(refresh);
+        if (!category.equals("refresh")) {
+
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        Boolean isExist = refreshRepository.existsByRefresh(refresh);
+        if (!isExist) {
+
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        // 검증 완료 -> 로그아웃 진행
+        // 해당 리프레쉬 토큰으로 refresh 테이블에서 관련 정보 삭제
+        refreshRepository.deleteByRefresh(refresh);
+
+        // 리프레쉬 토큰의 쿠키 값 비움
+        Cookie cookie = new Cookie("refresh", null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/"); // 쿠키가 브라우저에서 유효하게 사용될 수 있는 경로 범위 지정
+//        cookie.setSecure(true); // HTTPS 환경에서만 전송
+//        cookie.setHttpOnly(true); // JavaScript 접근 방지
+
+        response.addCookie(cookie);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+}
+
