91470669fbb6248c8e72b069dbddd58fe8381388
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-02-18 15:26:18 +0900

    refactor: try catch 제거

--- src/main/java/com/gdgoc/study_group/round/api/RetrospectController.java
+++ src/main/java/com/gdgoc/study_group/round/api/RetrospectController.java
@@ -21,7 +21,6 @@ public class RetrospectController {
 
     private final RetrospectService retrospectService;
 
-    //TODO: 예외 처리 구체화
     @Operation(summary = "회고 작성", description = "스터디를 참여한 멤버들이 회고를 작성합니다.")
     @PostMapping
     public ResponseEntity<Map<String, Long>> createRetrospect(
@@ -29,13 +28,8 @@ public class RetrospectController {
             @Parameter(description = "현재 인증된 사용자 정보", hidden = true) @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestBody RetrospectRequest request) {
 
-        try {
-            return ResponseEntity.ok(
-                    Map.of("round_member_id", retrospectService.createRetrospect(roundId, userDetails.getUsername(), request)));
-        } catch (CustomException e) {
-            // ROUND 또는 MEMBER가 존재하지 않거나 이미 회고가 존재하는 경우
-            return ResponseEntity.badRequest().build();
-        }
+        return ResponseEntity.ok(
+                Map.of("round_member_id", retrospectService.createRetrospect(roundId, userDetails.getUsername(), request)));
     }
 
     @Operation(summary = "회고 수정", description = "작성한 회고를 수정합니다.")
@@ -45,14 +39,8 @@ public class RetrospectController {
             @Parameter(description = "현재 인증된 사용자 정보", hidden = true) @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestBody RetrospectRequest request) {
 
-        try{
-            return ResponseEntity.ok(
-                    Map.of("round_member_id", retrospectService.updateRetrospect(roundId, userDetails.getUsername(), request)));
-        } catch (CustomException e) {
-            // 회고가 존재하지 않는 경우
-            return ResponseEntity.notFound().build();
-        }
-
+        return ResponseEntity.ok(
+                Map.of("round_member_id", retrospectService.updateRetrospect(roundId, userDetails.getUsername(), request)));
     }
 
     @Operation(summary = "회고 삭제", description = "회고를 삭제합니다.")
@@ -60,13 +48,9 @@ public class RetrospectController {
     public ResponseEntity<Void> deleteRetrospect(
             @Parameter(description = "회차 ID" ,required = true) @PathVariable Long roundId,
             @Parameter(description = "현재 인증된 사용자 정보", hidden = true) @AuthenticationPrincipal CustomUserDetails userDetails) {
-        try {
-            retrospectService.deleteRetrospect(roundId, userDetails.getUsername());
-            return ResponseEntity.noContent().build();
-        } catch (CustomException e) {
-            // 회고가 존재하지 않는 경우
-            return ResponseEntity.notFound().build();
-        }
+
+        retrospectService.deleteRetrospect(roundId, userDetails.getUsername());
+        return ResponseEntity.noContent().build();
     }
 
     @Operation(summary = "회고 조회", description = "특정 라운드의 모든 회고를 조회합니다. `?filter=memberId` 파라미터를 사용하면 특정 멤버의 회고만 조회할 수 있습니다.")
@@ -75,15 +59,11 @@ public class RetrospectController {
             @Parameter(description = "회차 ID" ,required = true) @PathVariable Long roundId,
             @RequestParam(value = "filter", required = false) Long memberId) {
 
-        if (memberId != null) {
-            try {
-                return ResponseEntity.ok(retrospectService.getRetrospect(roundId, memberId));
-            } catch (CustomException e) {
-                // 회고가 존재하지 않는 경우
-                return ResponseEntity.notFound().build();
-            }
+        if (memberId != null) { // 특정 회고 조회
+            return ResponseEntity.ok(retrospectService.getRetrospect(roundId, memberId));
         }
 
+        // 전체 회고 조회
         return ResponseEntity.ok(retrospectService.getAllRetrospects(roundId));
     }
 }
